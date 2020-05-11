45a1899f234a59fec02e033dbe8690092e76ce8f
Nik Mirza <nik96mirza@gmail.com>
2020-05-11 13:49:11 +0800

    change of parameter

--- pay.web.js
+++ pay.web.js
@@ -1,7 +1,9 @@
 import qs from 'qs';
 
 export default class RaudhahPay {
-    async pay({ api_uri, redirect_uri, collection_id, due, ref1, ref2, customer, product }) {
+    async pay({ api_uri, redirect_uri, collection_id, due, ref1, ref2, 
+        customer:{ first_name, last_name, email, mobile }, 
+        product: { title, price, quantity } }) {
         
         let body = {
             collection_code:collection_id,
@@ -9,7 +11,8 @@ export default class RaudhahPay {
             due: due || moment().format('YYYY-MM-DD'),
             currency: "MYR",
             ref1, ref2,
-            customer, product
+            first_name, last_name, email, mobile, 
+            title, price, quantity
         };
 
         let url = `${api_uri}/raudhahpay/pay?${qs.stringify(body)}`;
